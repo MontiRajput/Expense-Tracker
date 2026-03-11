c14039151f2d56756eb71e2990d2c8116de44682
monti <mrrajput7082@gmail.com>
2026-03-11 16:20:12 +0530

    POSTGRESS UPDATE

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 
-//postgresql connection 
+//postgresql connection
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExpensesModule } from './expenses/expenses.module';
 import { ConfigModule } from '@nestjs/config';
@@ -9,21 +9,23 @@ import { AuthModule } from './auth/auth.module';
 import { Expense } from './expenses/entities/expense.entity';
 import { User } from './auth/entities/user.entity';
 
-
-
 @Module({
-
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  autoLoadEntities: true,
-  synchronize: true,
-  entities:[Expense,User]
-}), ExpensesModule, AuthModule],
-  controllers: [AppController]
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [Expense, User],
+    }),
+    ExpensesModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
 })
 export class AppModule {}
